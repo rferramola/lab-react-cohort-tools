b60f845e2c0d3aa28d528693b8c0e9c0a98ad896
Roxana Ferramola <ferramolafvm@gmail.com>
2024-11-17 20:55:51 +0100

    until iteration 3

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,5 +1,6 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
+import { Link } from "react-router-dom";
 
 function StudentDetailsPage() {
 
@@ -65,12 +66,14 @@ function StudentDetailsPage() {
               </p>
             </div>
 
-
+          
             {/* Back button */}
+          <Link to="/">
             <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
               Back
             </button>
-
+            </Link>
+      
           </>
         )}
       </div>
